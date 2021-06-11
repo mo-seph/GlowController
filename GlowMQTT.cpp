be52afdf5ae421dc9a6529f65ed7a126bcfd689f
mo-seph <github@mo-seph.com>
2021-06-11 20:39:56 +0200

    Brought MQTT out as a Feature

--- GlowMQTT.cpp
+++ GlowMQTT.cpp
@@ -1,43 +1,38 @@
-#ifndef GLOW_MQTT_H
-#define GLOW_MQTT_H
-
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include "GlowController.h"
 #include "ArduinoJson.h"
 
+#ifndef GLOW_MQTT_H
+#include "GlowMQTT.h"
+#endif
+
 //GlowController glowControl;
+
+//GlowController* MQTT_GLOW_CONTROL = NULL;
+
 
 /* Set up WiFi */
 
 /* Set up MQTT */
 WiFiClient espClient;
-PubSubClient client(espClient);
+static PubSubClient client(espClient);
 #define MSG_BUFFER_SIZE	(1000)
 char msg[MSG_BUFFER_SIZE];
 
 //const char* mqtt_server = "broker.mqtt-dashboard.com";
-const char* command_channel = "/leds/1/commands";
-const char* state_channel = "/leds/1/state";
+//const char* command_channel = "leds/1/commands";
+//const char* state_channel = "leds/1/state";
 
 long retry_time = 0;
-long connection_interval = 2000;
+long connection_interval = 500;
 
-GlowController* MQTT_GLOW_CONTROL = NULL;
+//GlowController* MQTT_GLOW_CONTROL = NULL;
 
 
 
-void setupWiFi(const char* ssid, const char* password) {
-  Serial.println("Setting up WiFi");
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Establishing connection to WiFi..");
-  }
-  Serial.println("Connected to network");
-  Serial.println(WiFi.localIP());
-}
 
+/*
 void sendJsonMQTT(DynamicJsonDocument doc ) {
   if( client.connected() ) {
     Serial.print("Sending state on ["); Serial.print(state_channel); Serial.println("]");
@@ -47,6 +42,7 @@ void sendJsonMQTT(DynamicJsonDocument doc ) {
     Serial.println("Skipping MQTT output - not connected");
   }
 }
+*/
 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
@@ -56,23 +52,27 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-  MQTT_GLOW_CONTROL->update(payload,length);
+  MQTTConnector::gc_s->update(payload,length);
 }
 
+GlowController* MQTTConnector::gc_s;
 
+/*
 void setupMQTT(GlowController* c, const char * mqtt_server, int mqtt_port) {
   client.setBufferSize(1000);
   MQTT_GLOW_CONTROL = c;
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
 }
+*/
 
 
-void mqttConnect() {
   /*
   Try making a connection if we don't have one, but don't wait, get ready
   to try again later
   */
+  /*
+void mqttConnect() {
   if(!client.connected() ) {
     if( retry_time < millis() ) {
       Serial.print("Attempting MQTT connection...");
@@ -96,10 +96,12 @@ void mqttConnect() {
     }
   }
 
-  /* IFF we have a connection, then run the MQTT loop */
+  // IFF we have a connection, then run the MQTT loop
   if( client.connected() )   client.loop();
 }
+*/
 
+/*
 void setupGlowMQTT(GlowController* gc,
     const char* ssid, const char* pw, const char* server, int port = 1883)
   {
@@ -107,5 +109,4 @@ void setupGlowMQTT(GlowController* gc,
     setupMQTT(gc,server,port);
     mqttConnect();
   }
-
-#endif
+  */
--- GlowMQTT.h
+++ GlowMQTT.h
@@ -5,15 +5,132 @@
 #include <PubSubClient.h>
 #include "GlowController.h"
 #include "ArduinoJson.h"
+#include "GlowInterfaces.h"
 
-void setupWiFi();
-void sendJsonMQTT(DynamicJsonDocument doc );
+#define MSG_BUFFER_SIZE	(1000)
+
+
+
+//GlowController* MQTT_GLOW_CONTROL;
+
+//void setupWiFi(const char* ssid, const char* password);
+//void sendJsonMQTT(DynamicJsonDocument doc );
+//void setupMQTT(GlowController* c);
+//void mqttConnect();
+//void setupGlowMQTT(GlowController* gc, const char* ssid, const char* pw, const char* server, int port);
+
 void callback(char* topic, byte* payload, unsigned int length);
-void setupMQTT(GlowController* c);
-void mqttConnect();
-void setupGlowMQTT(GlowController* gc, const char* ssid, const char* pw, const char* server, int port);
 
 
+
+
+class MQTTConnector : public Connector {
+public:
+  MQTTConnector(PubSubClient *client, const char* mqtt_server, int mqtt_port) :
+    Connector(), client(client), mqtt_server(mqtt_server), mqtt_port(mqtt_port)
+      //command_channel("leds/1/commands"), state_channel("leds/1/state")
+      {
+      Serial.println("Creating MQTT Connector");
+      Serial.println("Setting buffer size");
+      client->setBufferSize(1000);
+      Serial.print("Setting server [");
+      Serial.print(mqtt_server);
+      Serial.print("] and port [");
+      Serial.print(mqtt_port);
+      Serial.println("]");
+      client->setServer(mqtt_server, mqtt_port);
+      updateConnection();
+
+  }
+  static GlowController* gc_s;
+
+  /* Should call super; lazy... */
+  virtual void setController(GlowController *c) {
+    Serial.println("Setting controller for MQTT Connector");
+    controller = c;
+    MQTTConnector::gc_s = c;
+    client_id = String("GlowLight-") + c->getID();
+    command_channel = String("leds/") + c->getID() + "/commands";
+    state_channel = String("leds/") + c->getID() + "/state";
+    ping_msg = String("{\"connected\":\"") + controller->getID() + "\"}";
+
+
+    client->setCallback(callback);
+    client->subscribe(command_channel.c_str());
+    Serial.println("Set callback function on client");
+  };
+
+  void updateConnection() {
+    /*
+    Try making a connection if we don't have one, but don't wait, get ready
+    to try again later
+    */
+    if(!client->connected() ) {
+      if( retry_time < millis() ) {
+        Serial.print("Attempting MQTT connection...");
+        // Attempt to connect
+        if (client->connect(client_id.c_str())) {
+          Serial.print("MQTT connected publishing on ");
+          Serial.println(state_channel);
+          // Once connected, publish an announcement...
+          ping();
+          // ... and resubscribe
+          client->subscribe(command_channel.c_str());
+        } else {
+          Serial.print("failed, rc=");
+          Serial.print(client->state());
+          Serial.println(" try again in 2 seconds");
+        }
+      } else {
+        retry_time = millis() + connection_interval;
+      }
+    }
+
+  }
+
+  void ping() {
+    if( client->connected() ) {
+      client->publish(state_channel.c_str(), ping_msg.c_str());
+    }
+  }
+
+
+  virtual void update(DynamicJsonDocument doc) {
+    updateConnection();
+    /* IFF we have a connection, then run the MQTT loop */
+    if( client->connected() )   client->loop();
+  }
+
+  virtual void outputState(JsonVariant v) {
+    if( client->connected() ) {
+      Serial.print("Sending state on ["); Serial.print(state_channel); Serial.println("]");
+      serializeJson(v, msg);
+      client->publish(state_channel.c_str(), msg);
+    } else {
+      Serial.println("Skipping MQTT output - not connected");
+    }
+  }
+protected:
+  //WiFiClient wifi_client;
+  PubSubClient *client;
+
+  String client_id = "GlowLight-";
+  String command_channel = "leds/";
+  String state_channel = "leds/";
+  String ping_msg = String("{\"connected\":0}");
+
+
+  char msg[MSG_BUFFER_SIZE];
+  //const char* mqtt_server = "broker.mqtt-dashboard.com";
+  const char* mqtt_server;
+  int mqtt_port;
+  //const char* command_channel;
+  //const char* state_channel;
+
+  long retry_time = 0;
+  long connection_interval = 2000;
+
+};
 
 
 #endif
