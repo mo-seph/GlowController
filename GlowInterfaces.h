9c5576b7ccd5f69202274c220fb9b32e3eb10d6b
mo-seph <github@mo-seph.com>
2021-06-11 20:40:45 +0200

    New Features interface for useful background features

---
+++ GlowInterfaces.h
@@ -0,0 +1,49 @@
+#ifndef GLOW_FEATURE_H
+#define GLOW_FEATURE_H
+
+#include "GlowController.h"
+#include "ArduinoJson.h"
+
+class GlowController;
+
+class Feature {
+public:
+  Feature() {};
+  virtual void update();
+  void setController(GlowController *c) {
+    controller = c;
+  };
+
+protected:
+  GlowController *controller;
+};
+
+class Connector {
+public:
+  Connector() {};
+  virtual void update(DynamicJsonDocument doc) {};
+  virtual void outputState(JsonVariant v) {};
+
+  virtual void setController(GlowController *c) {
+    controller = c;
+  };
+
+  bool checkDeserialisation(DeserializationError error) {
+    // Test if parsing succeeds.
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return false;
+    } else {
+      Serial.println("Got JSON input");
+      return true;
+    }
+  }
+
+protected:
+  GlowController *controller;
+};
+
+
+
+#endif
